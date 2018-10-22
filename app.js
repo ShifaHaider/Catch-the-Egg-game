f4d579bb8ff3dbba7af9b4afbe00360137114fcf
Shifa Haider <shifa.haider065@gmail.com>
2018-10-22 20:07:34 +0500

    complete game!

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function simpleEgg() {
     var eggImg = document.createElement('img');
     var left1 = Math.floor(Math.random() * (window.screen.width - 60));
     eggImg.setAttribute('src', 'egg2.png');
-    eggImg.style.height = '45px';
+    //eggImg.style.height = '45px';
     eggImg.style.width = '32px';
     eggImg.setAttribute('class', 'egg');
     egg1.appendChild(eggImg);
